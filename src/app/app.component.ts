d2d7f1d9ff506a073cbf5148b7115c1eb1bd1f94
Leon Ormes <leonormes@gmail.com>
2017-09-08 12:26:50 +0100

    Init basic form for uploading new audio files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  addWord(word: HTMLInputElement, file: HTMLInputElement): boolean {
+    console.log(`Adding word: ${word.value} & ${file.value}`);
+    return false;
+  }
 }
