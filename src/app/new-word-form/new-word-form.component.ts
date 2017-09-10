2e395c841658cd7fd77c7772a76222c6e38052fd
Leon Ormes <leonormes@gmail.com>
2017-09-10 22:26:49 +0100

    The user can now enter data about a new word
    Using ng's formControls and formGroups the controller and view now work
    well together collecting and validating user input.

--- src/app/new-word-form/new-word-form.component.ts
+++ src/app/new-word-form/new-word-form.component.ts
@@ -1,18 +1,35 @@
-import { Input, Component, OnInit } from '@angular/core';
-import { Phonic } from "../phonic.model";
-import { AppComponent } from "../app.component";
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, Validators, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
-  selector: 'app-new-word-form',
+  selector: 'new-word-form',
   templateUrl: './new-word-form.component.html',
   styleUrls: ['./new-word-form.component.css']
 })
 export class NewWordFormComponent implements OnInit {
-    @Input() phonic: Phonic;
+  wordForm: FormGroup;
+  word: AbstractControl;
+  file: AbstractControl;
+  meta: AbstractControl;
 
-  constructor() { }
+  constructor(fb: FormBuilder) {
+    this.wordForm = fb.group({
+      'word': ['', Validators.required],
+      'file': ['', Validators.required],
+      'meta': ['', Validators.required]
+    });
+
+    this.word = this.wordForm.controls['word']
+    this.file = this.wordForm.controls['file']
+    this.meta = this.wordForm.controls['meta']
+  }
 
   ngOnInit() {
+  }
+
+  onSubmit(form: string): void {
+    console.log('you submitted: ', form)
+    this.wordForm.reset();
   }
 
 }
