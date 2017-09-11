569a0555a730c3782047a2e6257a0632dd0177ae
Leon Ormes <leonormes@gmail.com>
2017-09-11 19:22:52 +0100

    merge

---
+++ src/app/phonic-list/phonic-list.component.ts
@@ -0,0 +1,17 @@
+import { Input, Component, OnInit } from '@angular/core';
+import { Phonic } from '../phonic.model';
+
+@Component({
+  selector: 'app-phonic-list',
+  templateUrl: './phonic-list.component.html',
+  styleUrls: ['./phonic-list.component.css']
+})
+export class PhonicListComponent implements OnInit {
+  @Input() phonic: Phonic;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
