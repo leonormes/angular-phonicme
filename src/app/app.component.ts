878118fd8f95749cb6d4baac7d0a0467b18ae832
Leon Ormes <leonormes@gmail.com>
2017-09-16 16:12:22 +0100

    add some words

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,12 +11,27 @@ export class AppComponent {
 
   constructor() {
     this.phonics = [
-      new Phonic('Cat', '../assets/audio/cat.mp3', 'phase 2'),
-      new Phonic('Dog', '../assets/audio/cat.mp3', 'phase 1'),
-      new Phonic('Bed', '../assets/audio/cat.mp3', 'phase 2'),
-      new Phonic('Sun', '../assets/audio/cat.mp3', 'phase 3'),
-      new Phonic('Run', '../assets/audio/cat.mp3', 'phase 2'),
-      new Phonic('So', '../assets/audio/cat.mp3', 'phase 2, tricky'),
+      new Phonic('the', '../assets/audio/cat.mp3', 'tricky'),
+      new Phonic('and', '../assets/audio/cat.mp3', 'phase 1'),
+      new Phonic('a', '../assets/audio/cat.mp3', 'phase 2'),
+      new Phonic('to', '../assets/audio/cat.mp3', 'tricky'),
+      new Phonic('said', '../assets/audio/cat.mp3', 'tricky'),
+      new Phonic('in', '../assets/audio/cat.mp3', 'phase 2, tricky'),
+      new Phonic('he', '../assets/audio/cat.mp3', 'phase 2, tricky'),
+      new Phonic('I', '../assets/audio/cat.mp3', 'phase 2, tricky'),
+      new Phonic('of', '../assets/audio/cat.mp3', 'phase 2, tricky'),
+      new Phonic('it', '../assets/audio/cat.mp3', 'phase 2'),
+      new Phonic('was', '../assets/audio/cat.mp3', 'phase 2, tricky'),
+      new Phonic('you', '../assets/audio/cat.mp3', 'phase 2'),
+      new Phonic('they', '../assets/audio/cat.mp3', 'phase 2'),
+      new Phonic('on', '../assets/audio/cat.mp3', 'phase 2'),
+      new Phonic('she', '../assets/audio/cat.mp3', 'phase 2, tricky'),
+      new Phonic('is', '../assets/audio/cat.mp3', 'phase 2, tricky'),
+      new Phonic('for', '../assets/audio/cat.mp3', 'phase 2'),
+      new Phonic('at', '../assets/audio/cat.mp3', 'phase 2'),
+      new Phonic('his', '../assets/audio/cat.mp3', 'phase 2, tricky'),
+      new Phonic('but', '../assets/audio/cat.mp3', 'phase 2'),
+      new Phonic('that', '../assets/audio/cat.mp3', 'phase 2'),
     ];
   }
 
