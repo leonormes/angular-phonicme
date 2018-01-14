bec1c1985b69ddd408294e73dc7d5937769e19ec
Leon Ormes <leonormes@gmail.com>
2018-01-14 21:54:35 +0000

    add page not found for wrong urls

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,23 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { HttpModule } from "@angular/http";
-import { RouterModule, Routes } from "@angular/router";
-import { AppRoutingModule, routingComponents } from "./app.routing";
-import { HttpClientModule } from "@angular/common/http";
-import { HttpLinkModule, HttpLink } from "apollo-angular-link-http";
-import { InMemoryCache } from "apollo-cache-inmemory";
-import { ApolloModule, Apollo } from "apollo-angular";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+import { AppRoutingModule } from './app.routing';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloModule, Apollo } from 'apollo-angular';
 
-import { AppComponent } from "./app.component";
-import { WordModule } from "./word/word.module";
-import { PhonicModule } from "./phonic/phonic.module";
-import { HomeComponent } from "./home/home.component";
-import { NavigationComponent } from "./navigation/navigation.component";
+import { AppComponent } from './app.component';
+import { WordModule } from './word/word.module';
+import { PhonicModule } from './phonic/phonic.module';
+import { HomeComponent } from './home/home.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
-  declarations: [AppComponent, NavigationComponent],
+  declarations: [AppComponent, NavigationComponent, PageNotFoundComponent],
   imports: [
     BrowserModule,
     PhonicModule,
@@ -32,7 +33,7 @@ import { NavigationComponent } from "./navigation/navigation.component";
 })
 export class AppModule {
   constructor(apollo: Apollo, httpLink: HttpLink) {
-    const uri = "https://serene-sands-14266.herokuapp.com/graphql";
+    const uri = 'https://serene-sands-14266.herokuapp.com/graphql';
     const http = httpLink.create({ uri });
 
     apollo.create({
