0b6bc223db10a00639890f7c17f4a32f4bcbae7f
Leon Ormes <leonormes@gmail.com>
2018-01-14 11:09:10 +0000

    Event bind scheme select to retrieve schme id
    This scheme id can then be used to fetch data related to the scheme that
    the user has choosen. The scheme choice should be saved to the user
    account so they do not need to keep choosing each time they load the app

--- src/app/phonic/phonic-scheme-item/phonic-scheme-item.component.ts
+++ src/app/phonic/phonic-scheme-item/phonic-scheme-item.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-phonic-scheme-item',
-  templateUrl: './phonic-scheme-item.component.html',
-  styleUrls: ['./phonic-scheme-item.component.css']
+  selector: "app-phonic-scheme-item",
+  templateUrl: "./phonic-scheme-item.component.html",
+  styleUrls: ["./phonic-scheme-item.component.css"]
 })
 export class PhonicSchemeItemComponent implements OnInit {
+  @Input() phonicScheme: any;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  onSelectScheme(event) {
+    console.log(event.target.id);
   }
-
 }
--- src/app/phonic/phonic-schemes-list/phonic-schemes-list.component.ts
+++ src/app/phonic/phonic-schemes-list/phonic-schemes-list.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Apollo } from "apollo-angular";
+import { Observable } from "rxjs/Observable";
+import "rxjs/add/operator/map";
+import gql from "graphql-tag";
+
+const PhonicSchemesList = gql`
+  query PhonicsSchemeQuery {
+    phonicSchemes {
+      uuid
+      name
+      description
+    }
+  }
+`;
 
 @Component({
-  selector: 'app-phonic-schemes-list',
-  templateUrl: './phonic-schemes-list.component.html',
-  styleUrls: ['./phonic-schemes-list.component.css']
+  selector: "app-phonic-schemes-list",
+  templateUrl: "./phonic-schemes-list.component.html",
+  styleUrls: ["./phonic-schemes-list.component.css"]
 })
 export class PhonicSchemesListComponent implements OnInit {
+  loading: boolean;
+  phonicSchemesList: any;
 
-  constructor() { }
+  constructor(private apollo: Apollo) {}
 
   ngOnInit() {
+    this.apollo
+      .watchQuery<any>({
+        query: PhonicSchemesList
+      })
+      .valueChanges.subscribe(({ data }) => {
+        this.loading = data.loading;
+        this.phonicSchemesList = data.phonicSchemes;
+      });
   }
-
 }
