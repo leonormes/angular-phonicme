6860a4d49e71e2f47d14f7035b0fcbfae05d4321
Leon Ormes <leonormes@gmail.com>
2018-01-14 09:07:18 +0000

    Fetch schemes data
    Using Apollo library. The phonics module needs data from the DB and this
    commit fetches the user choices of schemes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,10 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { HttpModule } from "@angular/http";
 import { RouterModule, Routes } from "@angular/router";
 import { AppRoutingModule, routingComponents } from "./app.routing";
+import { HttpClientModule } from "@angular/common/http";
+import { HttpLinkModule, HttpLink } from "apollo-angular-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloModule, Apollo } from "apollo-angular";
 
 import { AppComponent } from "./app.component";
 import { WordModule } from "./word/word.module";
@@ -19,8 +23,21 @@ import { NavigationComponent } from "./navigation/navigation.component";
     FormsModule,
     ReactiveFormsModule,
     HttpModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    HttpLinkModule,
+    ApolloModule
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+  constructor(apollo: Apollo, httpLink: HttpLink) {
+    const uri = "https://serene-sands-14266.herokuapp.com/graphql";
+    const http = httpLink.create({ uri });
+
+    apollo.create({
+      link: http,
+      cache: new InMemoryCache()
+    });
+  }
+}
--- src/app/phonic/phonic-home/phonic-home.component.ts
+++ src/app/phonic/phonic-home/phonic-home.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Apollo } from "apollo-angular";
+import { Observable } from "rxjs/Observable";
+import "rxjs/add/operator/map";
+import gql from "graphql-tag";
+
+const PhonicSchemesList = gql`
+  query PhonicsSchemeQuery {
+    phonicSchemes {
+      uuid
+      name
+      description
+    }
+  }
+`;
 
 @Component({
-  selector: 'app-phonic-home',
-  templateUrl: './phonic-home.component.html',
-  styleUrls: ['./phonic-home.component.css']
+  selector: "app-phonic-home",
+  templateUrl: "./phonic-home.component.html",
+  styleUrls: ["./phonic-home.component.css"]
 })
 export class PhonicHomeComponent implements OnInit {
+  loading: boolean;
+  phonicSchemesList: any;
 
-  constructor() { }
+  constructor(private apollo: Apollo) {}
 
   ngOnInit() {
+    this.apollo
+      .watchQuery<any>({
+        query: PhonicSchemesList
+      })
+      .valueChanges.subscribe(({ data }) => {
+        this.loading = data.loading;
+        this.phonicSchemesList = data.phonicSchemes;
+      });
   }
-
 }
