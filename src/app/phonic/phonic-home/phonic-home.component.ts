e2355d99fef45dfbf6f7aa7375d65d6c686116dc
Leon Ormes <leonormes@gmail.com>
2018-01-25 14:25:30 +0000

    Go straight to cardSets if user has already choosen phonisScheme

--- src/app/phonic/phonic-home/phonic-home.component.ts
+++ src/app/phonic/phonic-home/phonic-home.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UserService } from './../../sevices/user.service';
 @Component({
   selector: 'app-phonic-home',
   templateUrl: './phonic-home.component.html',
   styleUrls: ['./phonic-home.component.css']
 })
 export class PhonicHomeComponent implements OnInit {
-  constructor() {}
+  phonicSchemeId: any;
+  constructor(private userService: UserService) {
+    this.phonicSchemeId = userService.getPhonicSchemeId();
+  }
 
   ngOnInit() {}
 }
